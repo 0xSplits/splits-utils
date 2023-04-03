6ddefa768c949404e2b434b99f9b1b177ef03a88
Will Minshew <wminshew@gmail.com>
2023-04-03 14:51:54 -0700

    init

---
+++ src/WalletImpl.sol
@@ -0,0 +1,66 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity ^0.8.17;
+
+import {OwnableImpl} from "src/OwnableImpl.sol";
+
+/// @title WalletImpl
+/// @author 0xSplits
+/// @notice Bare bones smart wallet functionality
+abstract contract WalletImpl is OwnableImpl {
+    struct Call {
+        address to;
+        uint256 value;
+        bytes data;
+    }
+
+    event ExecCalls(Call[] calls);
+
+    /// -----------------------------------------------------------------------
+    /// storage - mutables
+    /// -----------------------------------------------------------------------
+
+    /// slot 0 - 12 bytes free
+
+    /// Owned storage
+    /// address public owner;
+    /// 20 bytes
+
+    /// -----------------------------------------------------------------------
+    /// constructor & initializer
+    /// -----------------------------------------------------------------------
+
+    constructor() {}
+
+    function __initWallet(address owner_) internal {
+        OwnableImpl.__initOwnable(owner_);
+    }
+
+    /// -----------------------------------------------------------------------
+    /// functions - external & public - onlyOwner
+    /// -----------------------------------------------------------------------
+
+    /// allow owner to execute arbitrary calls
+    function execCalls(Call[] calldata calls_)
+        external
+        payable
+        onlyOwner
+        returns (uint256 blockNumber, bytes[] memory returnData)
+    {
+        blockNumber = block.number;
+        uint256 length = calls_.length;
+        returnData = new bytes[](length);
+
+        bool success;
+        for (uint256 i; i < length;) {
+            Call calldata calli = calls_[i];
+            (success, returnData[i]) = calli.to.call{value: calli.value}(calli.data);
+            require(success, string(returnData[i]));
+
+            unchecked {
+                ++i;
+            }
+        }
+
+        emit ExecCalls(calls_);
+    }
+}
