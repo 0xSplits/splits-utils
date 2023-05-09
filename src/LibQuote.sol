d475401fded87b81eba46face6f4404096bf4f88
Will Minshew <wminshew@gmail.com>
2023-05-09 15:45:03 -0700

    add sorted quote pair struct & consolidate

--- src/LibQuote.sol
+++ src/LibQuote.sol
@@ -1,12 +1,26 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity ^0.8.17;
 
-using {_convert} for QuotePair global;
-using {_sort} for ConvertedQuotePair global;
+struct QuoteParams {
+    QuotePair quotePair;
+    uint128 baseAmount;
+    bytes data;
+}
 
 struct QuotePair {
     address base;
     address quote;
+}
+
+struct SortedQuotePair {
+    address token0;
+    address token1;
+}
+
+function _sort(QuotePair memory qp) pure returns (SortedQuotePair memory) {
+    return (qp.base > qp.quote)
+        ? SortedQuotePair({token0: qp.quote, token1: qp.base})
+        : SortedQuotePair({token0: qp.base, token1: qp.quote});
 }
 
 struct ConvertedQuotePair {
@@ -30,4 +44,11 @@ function _sort(ConvertedQuotePair memory cqp) pure returns (SortedConvertedQuote
     return (cqp.cBase > cqp.cQuote)
         ? SortedConvertedQuotePair({cToken0: cqp.cQuote, cToken1: cqp.cBase})
         : SortedConvertedQuotePair({cToken0: cqp.cBase, cToken1: cqp.cQuote});
+}
+
+function _convertAndSort(QuotePair calldata qp, function (address) internal view returns (address) convert)
+    view
+    returns (SortedConvertedQuotePair memory)
+{
+    return _sort(_convert(qp, convert));
 }
