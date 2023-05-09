92c44345d64558f4463818e83567c4881aa8bcf2
Will Minshew <wminshew@gmail.com>
2023-05-09 16:04:21 -0700

    rearrange files for global using

--- src/LibQuotes.sol
+++ src/LibQuotes.sol
@@ -1,6 +1,10 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity ^0.8.17;
 
+import {ConvertedQuotePair, SortedConvertedQuotePair} from "./ConvertedQuotePair.sol";
+
+using {_sort, _convert, _convertAndSort} for QuotePair global;
+
 struct QuoteParams {
     QuotePair quotePair;
     uint128 baseAmount;
@@ -23,16 +27,6 @@ function _sort(QuotePair memory qp) pure returns (SortedQuotePair memory) {
         : SortedQuotePair({token0: qp.base, token1: qp.quote});
 }
 
-struct ConvertedQuotePair {
-    address cBase;
-    address cQuote;
-}
-
-struct SortedConvertedQuotePair {
-    address cToken0;
-    address cToken1;
-}
-
 function _convert(QuotePair calldata qp, function (address) internal view returns (address) convert)
     view
     returns (ConvertedQuotePair memory)
@@ -40,15 +34,9 @@ function _convert(QuotePair calldata qp, function (address) internal view return
     return ConvertedQuotePair({cBase: convert(qp.base), cQuote: convert(qp.quote)});
 }
 
-function _sort(ConvertedQuotePair memory cqp) pure returns (SortedConvertedQuotePair memory) {
-    return (cqp.cBase > cqp.cQuote)
-        ? SortedConvertedQuotePair({cToken0: cqp.cQuote, cToken1: cqp.cBase})
-        : SortedConvertedQuotePair({cToken0: cqp.cBase, cToken1: cqp.cQuote});
-}
-
 function _convertAndSort(QuotePair calldata qp, function (address) internal view returns (address) convert)
     view
     returns (SortedConvertedQuotePair memory)
 {
-    return _sort(_convert(qp, convert));
+    return _convert(qp, convert)._sort();
 }
